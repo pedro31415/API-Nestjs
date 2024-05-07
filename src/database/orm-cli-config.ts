493f693cd2d66347fe170c6619a208b9ff775b48
Pedro <pedrogoku80@gmail.com>
2024-05-06 23:58:18 -0300

    feat: add modifed in file

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -1,10 +1,24 @@
-import { DataSource } from "typeorm";
-import { dataSourceOptions } from "./database.module";
+import 'dotenv/config'
+import { DataSource, DataSourceOptions } from "typeorm";
 import { CreateCoursesTable1714512358156 } from "src/migrations/1714512358156-CreateCoursesTable";
 import { CreateTagsTable1714514543519 } from "src/migrations/1714514543519-CreateTagsTable";
 import { CreateCoursesTagsTable1714570628128 } from "src/migrations/1714570628128-CreateCoursesTagsTable";
 import { AddCoursesIdToCoursesTagsTable1714571425309 } from "src/migrations/1714571425309-AddCoursesIdToCoursesTagsTable";
 import { AddTagsIdToCoursesTagsTable1714573022094 } from "src/migrations/1714573022094-AddTagsIdToCoursesTagsTable";
+import { Course } from "src/entities/courses.entity";
+import { Tag } from "src/entities/tags.entity";
+
+
+export const dataSourceOptions: DataSourceOptions = {
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    entities: [Course, Tag],
+    synchronize: false,
+}
 
 
 export const dataSource = new DataSource({
