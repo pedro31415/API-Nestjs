f70c05bed6d71a3a201194390bd489ed64792ba0
Pedro <pedrogoku80@gmail.com>
2024-05-06 23:57:24 -0300

    feat: user environment variable

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,27 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Course } from 'src/entities/courses.entity';
 import { Tag } from 'src/entities/tags.entity';
-import { DataSourceOptions } from 'typeorm';
 
-export const dataSourceOptions: DataSourceOptions = {
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'docker',
-    database: 'devtraining',
-    entities: [Course, Tag],
-    synchronize: false,
-}
+
 
 @Module({
     imports: [TypeOrmModule.forRootAsync({
-        useFactory: async () => {
+        useFactory: async (configService: ConfigService) => {
             return {
-                ... dataSourceOptions,
+                type: 'postgres',
+                host: configService.get('DB_HOST'),
+                port: configService.get('DB_PORT'),
+                username: configService.get('DB_USER'),
+                password: configService.get('DB_PASS'),
+                database: configService.get('DB_NAME'),
+                entities: [Course, Tag],
+                synchronize: false,
             }
-        }
+        },
+        inject: [ConfigService],
     })] 
 })
 export class DatabaseModule {}
