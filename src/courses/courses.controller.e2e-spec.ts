05802942b84a7af13bee3f800187f79a5c093a17
Pedro <pedrogoku80@gmail.com>
2024-05-29 17:01:41 -0300

    feat: add test end-to-end

--- src/courses/courses.controller.e2e-spec.ts
+++ src/courses/courses.controller.e2e-spec.ts
@@ -5,8 +5,8 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 import { CoursesModule } from './courses.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import request from 'supertest';
-import { Course } from 'src/entities/courses.entity';
-import { Tag } from 'src/entities/tags.entity';
+import { Course } from '../entities/courses.entity';
+import { Tag } from '../entities/tags.entity';
 
 describe('CoursesController test e2e', () => {
   let app: INestApplication
@@ -54,13 +54,82 @@ describe('CoursesController test e2e', () => {
     await dataSource.destroy()
   })
 
+  afterAll(async () => {
+    await module.close()
+  })
+
   describe('POST / courses', () => {
-    it('should be defined', async () => {
+    it('should create a course', async () => {
       const res = await request(app.getHttpServer())
-      .post('/courses')
-      .send(data)
-      expect(201)
+        .post('/courses')
+        .send(data)
+        .expect(201);
       console.log(res.body)
+      expect(res.body.id).toBeDefined()
+      expect(res.body.name).toEqual(data.name)
+      expect(res.body.description).toEqual(data.description)
+      expect(res.body.created_at).toBeDefined()
+      expect(res.body.tags[0].name).toEqual(data.tags[0])
+      expect(res.body.tags[1].name).toEqual(data.tags[1])
+    });
+  })
+
+  describe('GET / courses', () => {
+    it('should list all courses', async () => {
+      const res = await request(app.getHttpServer())
+        .get('/courses')
+        .expect(200);
+      expect(res.body[0].id).toBeDefined()
+      expect(res.body[0].name).toEqual(data.name)
+      expect(res.body[0].description).toEqual(data.description)
+      expect(res.body[0].created_at).toBeDefined()
+      res.body.map(item => expect(item).toEqual({
+        id: item.id,
+        name: item.name,
+        description: item.description,
+        created_at: item.created_at,
+        tags: [...item.tags]
+      }));
+    });
+  })
+
+  describe('GET /courses/:id', () => {
+    it('should gets a course by id', async () => {
+      const res = await request(app.getHttpServer())
+        .get(`/courses/${courses[0].id}`)
+        .expect(200);
+      expect(res.body.id).toEqual(courses[0].id)
+      expect(res.body.name).toEqual(courses[0].name)
+      expect(res.body.description).toEqual(courses[0].description)
+    });
+  })
+
+  describe('PUT /courses/:id', () => {
+    it('should update a course', async () => {
+      const updateData = {
+        name: 'new name',
+        description: 'new description',
+        tags: ['one', 'two']
+      }
+      const res = await request(app.getHttpServer())
+        .put(`/courses/${courses[0].id}`)
+        .send(updateData)
+        .expect(200);
+      expect(res.body.id).toEqual(courses[0].id)
+      expect(res.body.name).toEqual('new name')
+      expect(res.body.description).toEqual('new description')
+      expect(res.body.tags).toHaveLength(2)
+      expect(res.body.tags[0].name).toEqual('one')
+      expect(res.body.tags[1].name).toEqual('two')
+    });
+  })
+
+  describe('DELETE /courses/:id', () => {
+    it('delete a course', async () => {
+      const res = await request(app.getHttpServer())
+        .delete(`/courses/${courses[0].id}`)
+        .expect(204)
+        .expect({})
     });
   })
 });
