c0b05bcba78252e9ae0a44071d680d3f52080248
Pedro <pedrogoku80@gmail.com>
2024-06-04 14:14:15 -0300

    feat: correct import

--- src/courses/courses.module.ts
+++ src/courses/courses.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { CoursesController } from './courses.controller';
 import { CoursesService } from './courses.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Course } from 'src/entities/courses.entity';
-import { Tag } from 'src/entities/tags.entity';
+import { Course } from '../entities/courses.entity';
+import { Tag } from '../entities/tags.entity';
 
 @Module({
     imports: [TypeOrmModule.forFeature([Course,Tag])],
